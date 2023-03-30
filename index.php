98529601a72ee6fb03d694297027133fcc49dea9
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-03-30 16:33:55 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,279 +1,167 @@
+<?php
+  include_once("rotas.php");
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Busca de Filmes</title>
-    <link rel="icon" href="img/pop-corn.png">
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <style>
-        * {
-  margin: 0px;
-}
-html{
-  scroll-behavior: smooth;
-}
-body {
-  background-color: gray;
-  color: white;
-}
-
-.volta {
-  position: absolute;
-  top: 20px;
-  left: 50px;
-  width: 50px;
-  height: 50px;
-  background-color: white;
-  border-radius: 50%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  transition: all 0.5s;
-}
-.volta img {
-  width: 40px;
-  height: 40px;
-}
-
-.volta:hover {
-  scale: 1.1;
-}
-
-header {
-  /* background-color: green; */
-  background-image: url(img/backgrounds-Shrek-1893571-wallhere.com.jpg);
-  background-size: cover;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-header form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-header form input {
-  margin: 10px;
-  font-size: 20px;
-  padding: 10px;
-  border-radius: 10px;
-}
-header form input:nth-child(1) {
-  width: 40vw;
-  text-align: center;
-  border: none;
-}
-header form input:nth-child(1):focus {
-  outline: none;
-}
-header form input:nth-child(2) {
-  width: 150px;
-  transition: all 0.5s;
-  cursor: pointer;
-}
-header form input:nth-child(2):hover {
-  background-color: gray;
-}
-
-main {
-  height: 100vh;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-}
-main div {
-  width: 250px;
-  height: 170px;
-  margin: 20px;
-  cursor: pointer;
-  position: relative;
-}
-main div img {
-  border-radius: 20px;
-  width: inherit;
-  height: inherit;
-  transition: all 0.5s;
-  object-fit: cover;
-}
-main div .nm {
-  margin-top: -170px;
-  margin-left: -7px;
-  transition: all 0.5s;
-  background-color: green;
-  border-radius: 20px;
-  padding: 10px;
-  width: 250px;
-  opacity: 0;
-  position: absolute;
-}
-main div .nm spam{
-  text-transform: uppercase;
-  text-decoration-line: underline;
-}
-main div .dr{
-  position: absolute;
-  top: 0px;
-  left: 5px;
-  background-color: green;
-  padding: 5px;
-  border-radius: 10px;
-  opacity: 0;
-  z-index: 999;
-  transition: all 0.5s;
-}
-main div .cl{
-  position: absolute;
-  top: 0px;
-  right: 5px;
-  background-color: green;
-  padding: 5px;
-  border-radius: 10px;
-  opacity: 0;
-  z-index: 999;
-  transition: all 0.5s;
-}
-main div:hover img {
-  scale: 1.1;
-}
-main div:hover .nm {
-  margin-top: 0px;
-  opacity: 1;
-  z-index: 999;
-}
-main div:hover .dr {
-  margin-top: 0px;
-  z-index: 999;
-  opacity: 1;
-}main div:hover .cl {
-  opacity: 1;
-}
-
-.cont{
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 200px;
-  position: absolute;
-  top: 20px;
-  right: 10px;
-}
-.cont a img{
-  width: 50px;
-}
-@media (max-width: 350px){
-  .cont{
-    width: 40vw;
-  }
-  .top{
-    display: none;
-  }
-}
-@media (max-width: 500px){
-  header form input:nth-child(1) {
-    width: 80vw;
-    text-align: center;
-    border: none;
-  }
-  .top img{
-    width: 8vw !important;
-  }
-}
-.top{
-  position: fixed;
-  bottom: 50px;
-  right: 10px;
-  background-color: greenyellow;
-  border-radius: 50%;
-  z-index: 99;
-  transition: all 0.5s;
-}
-.top img{
-  width: 50px;
-  rotate: 90deg;
-  padding: 5px;
-}
-.top:hover{
-  scale: 1.1;
-}
-    </style>
-
+    <title>Doc Hudson</title>
+    <link rel="icon" href="Frontend/FrontendPages/img/Capturar-removebg-preview 1.png">
+    <link rel="stylesheet" href="Frontend/header.css">
+    <link rel="stylesheet" href="Frontend/cssHomePage/home.css">
+    <link rel="stylesheet" href="Frontend/cssHomePage/marketing.css">
+    <link rel="stylesheet" href="Frontend/cssHomePage/blog.css">
+    <link rel="stylesheet" href="Frontend/cssHomePage/localiza.css">
+    <link rel="stylesheet" href="Frontend/cssHomePage/instituicao.css">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+    crossorigin="anonymous">
 </head>
-<body>
-    <a href="index.php" class="volta">
-        <img src="img/left-arrow.png" alt="">
-    </a>
-    
-    <div class="cont">
-      <a href="https://github.com/MayckL2" target="_blank">
-        <img src="img/github.png" alt="">
-      </a>
-      <a href="https://www.linkedin.com/in/mayck-luciano-993403171/" target="_blank">
-        <img src="img/linkedin.png" alt="">
-      </a>
-    </div>
-
-    <a href="#top" class="top">
-      <img src="img/left-arrow.png" alt="">
+<body onresize="checaDispositivo()" onload="checaDispositivo()" onscroll="escondeHeader()">
+  
+    <a href="#topo" class="setaTopo">
+      <img src="Frontend/FrontendPages/img/right-arrow.png" alt="seta">
     </a>
 
-    <header id="top">
-        <form action="busca.php" method="post">
-            <input type="text" name="busca" placeholder="Pesquise um filme aqui">
-            <input type="submit">
-        </form>
+    <header>
+        <a href="" class="logo">
+            <img src="Frontend/FrontendPages/img/Capturar-removebg-preview 1.png" alt="logoEmpresa">
+        </a>
+
+        <img onclick="chamaMenu()" src="Frontend/FrontendPages/img/menu-suspenso.png" class="menu" alt="menu">
+
+        <ul class="retratil">
+            <li class="menuItens"><a href="Frontend\FrontendPages\mapa\mapa.html">Mapa de localização</a></li>
+            <li class="menuItens"><a href="">Blog</a></li>
+            <li class="menuItens"><a href="Frontend\FrontendPages\institucional\institucional.html">Institucional</a></li>
+            <li><a href="<?php echo $loginRoute; ?>">Login</a></li>
+        </ul>
     </header>
 
-    <main>
-        <?php
-        include "conexao.php";
+    <section class="home" id="topo">
+        <div class="box">
+            <div>
+                <h1>CONHEÇA NOSSA ESTRUTURA</h1>
+                <p>São Paulo - Capital</p>
+                <a href="">Veja</a>
+            </div>
+        </div>
+    </section>
 
-        $sql = "select * from filmes";
-        $res = mysqli_query($con, $sql);
+    <section class="container row mark">
+      
+      <div class="col-md">
+        <img src="Frontend/FrontendPages/img/ciber-seguranca 1.png" alt="segurança">
+        <h2>Segurança</h2>
+        <p>Mantemos seu carro segura enquanto você precisar</p>
+      </div>
+      
+      <div class="col-md">
+        <img src="Frontend/FrontendPages/img/localizacao 1.png" class="maior" alt="localizacao">
+        <h2>Localização</h2>
+        <p>localização próxima ao seus objetivos</p>
+      </div>
+      
+      <div class="col-md">
+        <img src="Frontend/FrontendPages/img/transferencia-movel 1.png" alt="preço">
+        <h2>Preço</h2>
+        <p>Preços acessiveis e justos para toda a população</p>
+      </div>
 
-        //acessa coluna pelo nome
-        while($vreg= mysqli_fetch_assoc($res)){
-            $id = $vreg['id'];
-            $nome = $vreg['nome'];
-            $sinopse = $vreg['sinopse'];
-            $classif = $vreg['classificacao'];
-            if ($classif == 0){
-            $bk = "green";
-            $m = "Livre";
-            }elseif($classif == 10){
-            $bk = "blue";
-            $m = $classif;
-            }elseif($classif == 12){
-            $bk = "gold";
-            $m = $classif;
-            }elseif($classif == 14){
-            $bk = "orange";
-            $m = $classif;
-            }elseif($classif == 16){
-            $bk = "red";
-            $m = $classif;
-            }else{
-            $bk = "black";
-            $m = $classif;
-            }
+    </section>
 
-            $duracao = $vreg['duracao'];
+    <section class="container-fluid row map">
+      <div class="col-md centraGrid">
+        <div class="gridMap">
+          <img src="Frontend/FrontendPages/img/localizacao 2.png" id="item1" alt="seta">
+          <img src="Frontend/FrontendPages/img/Rectangle 59.png" id="item2" alt="cidade">
+          <img src="Frontend/FrontendPages/img/Rectangle 61.png" id="item3" alt="cidade2">
+        </div>
+      </div>
 
-            // echo "<h1>$id - $nome</h1>";
-            echo "<div>
-                    <img src='img/". $nome .".jpg'>
-                    <p class='cl' style='background-color: $bk'>$m</p>
-                    <p class='dr'>$duracao Min</p>
-                    <p class='nm'><spam>$nome</spam><br>$sinopse</p>
-                  </div>";
-        }
+      <div class="col-md descMap">
+        <h2>MAPA DE LOCALIZAÇÃO NA CIDADE DE SÃO PAULO</h2>
+        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os</p>
+        <a href="">MAPA</a>
+      </div>
+    </section>
 
-        $con -> close();
-        ?>
-    </main>
+    <section class="container-fluid row blog">
+      <div class="col-md carImg">
+        <div></div>
+        <img src="Frontend/FrontendPages/img/image-removebg-preview 1.png" alt="carro">
+      </div>
 
+      <div class="col-md descBlog">
+        <h2>CONHEÇA NOSSO BLOG</h2>
+        <p>Este é o nosso espaço com dicas, notícias, novidades e informações úteis sobre
+          os principais eventos que contam com nossos estacionamentos, mobilidade urbana, e mais!</p>
+        <a href="">Saiba mais</a>
+      </div>
+
+      <!-- <div class="col-md barra">
+        <div></div>
+      </div> -->
+    </section>
+
+    <section class="container-fluid row inst m-0">
+      <div>
+        <h2>SOBRE A DOC HUDSON</h2>
+        <p>Buscamos através da inovação tecnologica a maior segurança e comobidade para o seu carro
+          em uma localização coringa para o dia a dia em São Paulo</p>
+        <a href="">Saiba Mais</a>
+      </div>
+    </section>
+
+    <div class="contato">
+      <button class="fecha">X</button>
+      <h2>ENTRE EM CONTATO</h2>
+
+      <p>Se você ficou interessado, preencha o formulário abaixo e entre para a rede de estacionamentos que é líder no mercado.</p>
+
+      <form action="">
+        <fieldset id="email">
+          <legend>E-mail</legend>
+          <input type="email">
+        </fieldset>
+        <fieldset id="ddd">
+          <legend>DDD</legend>
+          <input type="text">
+        </fieldset>
+        <fieldset id="tell">
+          <legend>Telefone</legend>
+          <input type="text">
+        </fieldset>
+        <fieldset id="mensa">
+          <legend>Mensagem</legend>
+          <textarea name="" id="" cols="10" rows="10"></textarea>
+        </fieldset>
+
+        <button>Enviar</button>
+      </form>
+    </div>
+
+    <footer>
+      <div class="barra"></div>
+
+      <!-- <img src="Frontend\FrontendPages\img\Capturar-removebg-preview 1.png" alt="logo"> -->
+
+      <div>
+        <button>Contate-nos</button>
+        <a href=""><img src="Frontend\FrontendPages\img\facebook.png" alt="facebook"></a>
+        <a href=""><img src="Frontend\FrontendPages\img\github.png" alt="github"></a>
+        <a href=""><img src="Frontend\FrontendPages\img\insta.png" alt="instagram"></a>
+      </div>
+
+    </footer>
+
+    <script src="Frontend/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
 </body>
 </html>
